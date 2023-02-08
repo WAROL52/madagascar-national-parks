a085433db58b130fb05e3ce8624573ecdb3af5cd
WAROL52 <rabetsyrolio@gmail.com>
2023-02-08 13:21:23 +0100

    mise a jour de reg

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -9,36 +9,40 @@ export default async function handler(
 ) {
   console.log(req.method);
   console.log(req.body);
-  const { nom, prenom, email, motdepasse } = req.body;
-  const emailAccepted = await prisma.email.findUnique({
-    where: {
-      email,
-    },
-  });
-  if (emailAccepted) {
-    if (await prisma.user.findUnique({ where: { email } })) {
-      return res.status(200).json({
-        hasError: true,
-        type: 2,
-        title: "Email déjà enregistré!",
-        description: "Il semble que vous êtes déjà enregistré avec cet e-mail.",
-      });
-    }
-    const user = await prisma.user.create({
-      data: {
+  if (req.body == "POST") {
+    const { nom, prenom, email, motdepasse } = req.body;
+    const emailAccepted = await prisma.email.findUnique({
+      where: {
         email,
-        motdepasse,
-        nom,
-        prenom,
       },
     });
-    return res.status(200).json(user);
+    if (emailAccepted) {
+      if (await prisma.user.findUnique({ where: { email } })) {
+        return res.status(200).json({
+          hasError: true,
+          type: 2,
+          title: "Email déjà enregistré!",
+          description:
+            "Il semble que vous êtes déjà enregistré avec cet e-mail.",
+        });
+      }
+      const user = await prisma.user.create({
+        data: {
+          email,
+          motdepasse,
+          nom,
+          prenom,
+        },
+      });
+      return res.status(200).json(user);
+    }
+    res.status(200).json({
+      hasError: true,
+      title: "Email non valide!",
+      type: 1,
+      description:
+        "L'adresse e-mail que vous avez utilisée n'est pas autorisée à s'enregistrer sur cette plate-forme.",
+    });
   }
-  res.status(200).json({
-    hasError: true,
-    title: "Email non valide!",
-    type: 1,
-    description:
-      "L'adresse e-mail que vous avez utilisée n'est pas autorisée à s'enregistrer sur cette plate-forme.",
-  });
+  return await prisma.user.findMany();
 }
