a97eeab869ac0b2cdcf0cf02c5ba7871f806165d
WAROL52 <rabetsyrolio@gmail.com>
2023-02-08 20:05:28 +0100

    6em tentative

--- prisma/migrations/20230207031726_email/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Email" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "email" TEXT NOT NULL,
-    "nom" TEXT,
-    "prenom" TEXT
-);
