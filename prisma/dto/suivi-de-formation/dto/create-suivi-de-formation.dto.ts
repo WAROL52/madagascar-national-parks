c9e59c8336dbfc63fe8a69b75827d3373f3eec48
WAROL52 <rabetsyrolio@gmail.com>
2023-02-21 20:13:56 +0300

    correction du decorator

--- prisma/dto/suivi-de-formation/dto/create-suivi-de-formation.dto.ts
+++ prisma/dto/suivi-de-formation/dto/create-suivi-de-formation.dto.ts
@@ -1,14 +1,20 @@
-import { SiteName } from "@prisma/client";
+
+import {SiteName} from '@prisma/client'
+import {ApiProperty} from '@nestjs/swagger'
+
+
+
 
 export class CreateSuiviDeFormationDto {
-  siteName: SiteName;
-  tacheName: string;
-  progression?: number;
-  debutPrevionnel: Date;
-  finPrevisionnel: Date;
-  nombreDeJours: number;
-  debutReel?: Date;
-  finReel?: Date;
-  perturbation?: number;
-  tempsConsommes?: number;
+  @ApiProperty({ enum: SiteName})
+siteName: SiteName;
+tacheName: string;
+progression?: number;
+debutPrevionnel: Date;
+finPrevisionnel: Date;
+nombreDeJours: number;
+debutReel?: Date;
+finReel?: Date;
+perturbation?: number;
+tempsConsommes?: number;
 }
